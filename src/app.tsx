3b7a81922dc5bcf679f4f786b04f2ac99c192574
YanHSama <1269987538@qq.com>
2026-03-19 15:57:19 +0800

    feat: taro项目初始化

---
+++ src/app.tsx
@@ -0,0 +1,28 @@
+import { Component, PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
+
+import configStore from './store'
+
+import './app.styl'
+
+const store = configStore()
+
+class App extends Component<PropsWithChildren> {
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Provider store={store}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export default App
