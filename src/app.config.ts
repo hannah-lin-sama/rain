6f45f7c0fb94f9be75766bd40d7327e14f2a1be0
YanHSama <1269987538@qq.com>
2026-03-19 22:49:43 +0800

    feat: 添加路由

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,7 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/user/index'
   ],
   window: {
     backgroundTextStyle: 'light',
---
+++ src/pages/user/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "@tarojs/components";
+function User() {
+  return (
+    <View>
+      <Text>Hello, User</Text>
+    </View>
+  );
+}
+export default User;
